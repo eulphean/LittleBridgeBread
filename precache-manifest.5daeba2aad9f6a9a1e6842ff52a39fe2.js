4e3cab4633463ea77eb9faa9f7fc26fb4d88923a
Amay Kataria <amkatari@microsoft.com>
2020-04-27 10:28:32 -0500

    Updates

---
+++ precache-manifest.5daeba2aad9f6a9a1e6842ff52a39fe2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9a3d1749979277af84968e7908b7bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "589ef16e08a322d125f4",
+    "url": "/static/css/main.9e847670.chunk.css"
+  },
+  {
+    "revision": "2b8566ef5b229708c885",
+    "url": "/static/js/2.2976b0cd.chunk.js"
+  },
+  {
+    "revision": "90526b3367ca000531e0465e56eee9ab",
+    "url": "/static/js/2.2976b0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589ef16e08a322d125f4",
+    "url": "/static/js/main.125aa4a8.chunk.js"
+  },
+  {
+    "revision": "a6226f722fa50a6ac885",
+    "url": "/static/js/runtime-main.cccb6e54.js"
+  },
+  {
+    "revision": "6acdacdc31196f4007a5cc48e1340130",
+    "url": "/static/media/din-webfont.6acdacdc.woff2"
+  },
+  {
+    "revision": "e6a361ef75e47721d5e73bbb557d7635",
+    "url": "/static/media/din-webfont.e6a361ef.woff"
+  },
+  {
+    "revision": "430b6e66af48f52312b6dc040cd7d225",
+    "url": "/static/media/flatlogo.430b6e66.svg"
+  },
+  {
+    "revision": "82c2386ad1014d8234559507485f6c87",
+    "url": "/static/media/jamie_clarke_type_-_rig_solid_bold_halftone-webfont.82c2386a.woff2"
+  },
+  {
+    "revision": "b821cedfeeadfacd7cb7f1aafc664a29",
+    "url": "/static/media/jamie_clarke_type_-_rig_solid_bold_halftone-webfont.b821cedf.woff"
+  },
+  {
+    "revision": "fc856b1fda2dfeb36218f811e6cfcc98",
+    "url": "/static/media/leaf.fc856b1f.svg"
+  },
+  {
+    "revision": "87b33e2de3819ca5260319ef546738c9",
+    "url": "/static/media/opensans-light-webfont.87b33e2d.woff2"
+  },
+  {
+    "revision": "9ad03de81bbf6cd48ffc44cf2aa4d707",
+    "url": "/static/media/opensans-light-webfont.9ad03de8.woff"
+  },
+  {
+    "revision": "65a61f9230a85b44df43968f80a522e5",
+    "url": "/static/media/opensans-regular-webfont.65a61f92.woff"
+  },
+  {
+    "revision": "d407a58546593548175615d7621183ef",
+    "url": "/static/media/opensans-regular-webfont.d407a585.woff2"
+  },
+  {
+    "revision": "77f718e73b4b1dfeaed822715e2993ec",
+    "url": "/static/media/roundlogo.77f718e7.svg"
+  },
+  {
+    "revision": "8065638cc7e7f14fca82b40db1687366",
+    "url": "/static/media/sourdough.8065638c.jpg"
+  }
+]);
